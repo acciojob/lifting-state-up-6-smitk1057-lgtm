ab2f0ed18d2f959c84b47e5545ce72abfe7c4731
Sumit Kumar <smitk1057@gmail.com>
2026-05-19 12:03:25 +0530

    Lift

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,35 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
 import Child1 from "./Child1";
 
-const App = () => {
-  const [todos,setTodos]=useState([{id:1,text:"Learn React",complete:false},
-                                 {id:2,text:"Build a React app",complete:false},
-                                 {id:3,text:"Deploy the React app",complete:false}])
+function App() {
 
+  const [todos, setTodo] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Build React App", completed: false },
+    { id: 3, text: "Deploy React App", completed: false },
+  ]);
 
-  const handleComplete=(id)=>{
-    let handleTodo=todos.map((todo)=>
-      todo.id==id
-      ?{...todo, complete:true}
-      :todo
-    )
-    setTodos(handleTodo)
-  }
-  
+  const handleComplete = (id) => {
+
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id
+        ? { ...todo, completed: true }
+        : todo
+    );
+
+    setTodo(updatedTodos);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>Parent Component</h1>
-        <Child1 todos={todos}
-            handleComplete={handleComplete} />
+      <h1>Parent Component</h1>
+
+      <Child1
+        todos={todos}
+        handleComplete={handleComplete}
+      />
     </div>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
--- src/components/Child1.js
+++ src/components/Child1.js
@@ -1,28 +1,27 @@
-import React from "react"
+import React from "react";
 
-
-const Child1=({todos,handleComplete})=>{
-  
+const Child1 = ({ todos, handleComplete }) => {
   return (
-        
     <div>
-           <h2>Child Component</h2>
-          <ul>
-            {
-                todos.map((todo)=>(
-                    <li key={todo.id}>
-                        {todo.text} {todo.complete ?(""):""}
-                      {!todo.complete && (
-                        <button onClick={()=>handleComplete(todo.id)}>Complete</button>
-                      )}
-                    </li>
-                ))
-            }
-           </ul>
-           
+      <h2>Child Component</h2>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+
+            {todo.completed ?(
+              <span>{''}</span>
+            ) : (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
-    )
-}
+  );
+};
 
-export default Child1
-
+export default Child1;
